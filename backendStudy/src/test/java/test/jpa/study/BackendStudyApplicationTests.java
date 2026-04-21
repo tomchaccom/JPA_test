92054ec6fc94cdb4dc4fa9fb03a096242382a9a3
tomchccom <dreamkms2014@gmail.com>
2026-04-21 10:41:50 +0900

    initial commit

---
+++ backendStudy/src/test/java/test/jpa/study/BackendStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package test.jpa.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
